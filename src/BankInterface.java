b86b128f1e993da7ae105ecea9b83b12fe3dbe6c
Awadhesh kumar singh <awadheshmzp1997@gmail.com>
2023-01-13 12:23:22 +0530

    Add bank application

---
+++ src/BankInterface.java
@@ -0,0 +1,13 @@
+public interface BankInterface {
+
+    int checkBalance();
+
+
+    String addMoney(int money);
+
+    String withDrawMoney(int money, String password);
+
+
+    float calculateInterest(int year);
+
+}
